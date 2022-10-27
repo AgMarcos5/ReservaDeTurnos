352f9951e665ac39c10a3bdebfd201cdcda74e63
Marcos Aguero <aguermar@gmail.com>
2022-10-27 17:39:39 -0300

    front inicial

---
+++ clienteWeb/js/app.js
@@ -0,0 +1,47 @@
+import {getData} from './getData.js'
+
+let turnos = [];
+
+const initTurnos = (async () => {
+
+    turnos = await getData("http://localhost:3000/api/reserva")
+    console.log(turnos)
+    
+    turnos.map( turno => {
+
+        const ocupado = turno.userId != null ? 'ocupado' : '';
+
+        listaTurnos.innerHTML += `
+        <div 
+            class="turno ${ocupado}" 
+            id="${turno.id}"
+        >
+            <div>${turno.datetime}</div>
+        </div>
+        `
+    })
+    
+    // Seleccionar turno
+    const turnosHTML = document.querySelectorAll('.turnosContainer .turno');
+    turnosHTML.forEach(turno => {
+        turno.addEventListener('click', () => {
+          document.querySelector(".turnosContainer .turno.selected")?.classList.remove("selected");
+          turno.classList.add("selected");
+        });
+    });
+    
+   
+}) ();
+
+
+
+
+reservaForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    reservarTurno();
+})
+
+const reservarTurno = () => {
+    const turno = document.querySelector(".turnosContainer .turno.selected").id
+    console.log("reservar turno", turnos[turno-1])
+}
