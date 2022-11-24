b970e12359c93202fdb8c9f25cc55cb2b0f488a5
tomfernandezp <tomfernandezp@gmail.com>
2022-11-24 13:07:16 -0300

    Mapas finalizados

--- servicios/gestionSucursales/src/config.js
+++ servicios/gestionSucursales/src/config.js
@@ -7,7 +7,7 @@ const config = {
     SUCURSALES_PORT : process.env.SUCURSALES_PORT || 3000,
     PATH_JSON: "/../../sucursales.json",
 
-    MAP_ID: "bb782eaf-5cd0-4895-bd42-423cb02ca4e8",
+    MAP_ID: "a7454b8d-a1f7-4732-8c35-7865f872e111",
     MAP_BASEURL: "https://cartes.io/api/maps",
     
 }
